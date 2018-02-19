9fbcf129d1e143f1300bbc49595eac8a6390b761
Andy Mai <hi@andymai.com>
2018-02-19 10:54:31 -0800

    prettify

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const config = {
-  'secrets': {
-    'clientId': process.env.FOURSQUARE_APIKEY,
-    'clientSecret': process.env.FOURSQUARE_SECRET,
-    'redirectUrl': 'http://isisraelinjapan.com'
-  }
+	secrets: {
+		clientId: process.env.FOURSQUARE_APIKEY,
+		clientSecret: process.env.FOURSQUARE_SECRET,
+		redirectUrl: 'http://isisraelinjapan.com'
+	}
 };
 // Use your account's OAUTH token. You need to be friends with Israel.
-const foursquareAccessToken = process.env.FOURSQUARE_TOKEN; 
+const foursquareAccessToken = process.env.FOURSQUARE_TOKEN;
 
 const foursquare = require('node-foursquare')(config);
 const express = require('express');
@@ -24,31 +24,31 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
 
 const checkLocation = () => {
-  // Get friends recent checkins
-  foursquare.Checkins.getRecentCheckins({}, foursquareAccessToken, function (err, checkins) {
-    if (err) throw new Error(err);
+	// Get friends recent checkins
+	foursquare.Checkins.getRecentCheckins({}, foursquareAccessToken, function(err, checkins) {
+		if (err) throw new Error(err);
 
-    checkins.recent.forEach(function (checkin) {
-      // Check if his most recent checkin was in Japan
-      if (checkin.user.id == 354825) {
-        return (checkin.venue.location.country == "Japan");
-      }
-    });
-  });
-  return false;
+		checkins.recent.forEach(function(checkin) {
+			// Check if his most recent checkin was in Japan
+			if (checkin.user.id == 354825) {
+				return checkin.venue.location.country == 'Japan';
+			}
+		});
+	});
+	return false;
 };
 
 app.get('/', (req, res) => {
-    var checkedIn = checkLocation();
-    var isHeInJapan = 'No';
-    if (checkedIn) isHeInJapan = 'Yes';
-    res.render(__dirname + '/index', { isHeInJapan: isHeInJapan });
+	var checkedIn = checkLocation();
+	var isHeInJapan = 'No';
+	if (checkedIn) isHeInJapan = 'Yes';
+	res.render(__dirname + '/index', { isHeInJapan: isHeInJapan });
 });
 
 app.get('/api', (req, res) => {
-  var checkedIn = checkLocation();
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ is: checkedIn }));
+	var checkedIn = checkLocation();
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({ is: checkedIn }));
 });
 
 app.listen(PORT, HOST);
